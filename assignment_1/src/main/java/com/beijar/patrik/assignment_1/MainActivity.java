74bb4fd59fa6711349b0023d15ad9ad5faaf1248
Beijar <patrikbeijar@gmail.com>
2015-09-10 12:43:00 +0200

    small fix

--- assignment_1/src/main/java/com/beijar/patrik/assignment_1/MainActivity.java
+++ assignment_1/src/main/java/com/beijar/patrik/assignment_1/MainActivity.java
@@ -3,14 +3,18 @@ package com.beijar.patrik.assignment_1;
 import android.support.v4.app.FragmentActivity;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends FragmentActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
         if (findViewById(R.id.fragment_container) != null) {
 
@@ -32,21 +36,25 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        Log.d(TAG, "onStart");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.d(TAG, "onPause");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Log.d(TAG, "onStop");
     }
 
     @Override
